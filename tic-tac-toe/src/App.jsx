890406f00bef5910c8322653c7b485904f5b4b6b
mfhsohag01 <md.sohag2001bd@gmail.com>
2024-12-18 12:57:22 +0600

    first commit

---
+++ tic-tac-toe/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+function Square({ value, onSquareClick }) {
+  return (
+    <button
+      className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg"
+      onClick={onSquareClick}
+    >
+      {value}
+    </button>
+  );
+}
+
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
+    const nextSquare = squares.slice();
+
+    nextSquares[0] = "X";
+    setSquares(nextSquares);
+  }
+
+  return (
+    <>
+      <div className="flex">
+        <Square value={squares[0]} onSquareClick={handleClick} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
+      </div>
+
+      <div className="flex">
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
+      </div>
+
+      <div className="flex">
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
+      </div>
+    </>
+  );
+}
